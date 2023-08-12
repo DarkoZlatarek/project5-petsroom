7ac9a9e7c61699d9c079fd6a079f8f02c8532cf4
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-12 20:28:06 +0000

    Add styles for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,33 @@
 import React from "react"
 import { Navbar, Container, Nav} from "react-bootstrap";
 import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="50" />
+          <Nav.Link>
+            <img className={styles.logo} src={logo} alt="logo" height="50" />
+            <spam className={styles.title}>Petshome</spam>
+          </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-house"></i>
+              <spam className={styles.NavText}>Home</spam>
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-right-to-bracket"></i>
+              <spam className={styles.NavText}>Sign in</spam>
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-user-plus"></i>
+              <spam className={styles.NavText}>Sign up</spam>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

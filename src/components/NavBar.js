bd7a1d6a02d056c4978c5c9d3be34a401cb51d46
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-12 21:07:12 +0000

    Add Route and wire up all pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,32 +2,46 @@ import React from "react"
 import { Navbar, Container, Nav} from "react-bootstrap";
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand>
-          <Nav.Link>
+        <NavLink to="/">
+          <Navbar.Brand>
             <img className={styles.logo} src={logo} alt="logo" height="50" />
-            <spam className={styles.title}>Petshome</spam>
-          </Nav.Link>
-        </Navbar.Brand>
+            <spam className={styles.title}>PetsRoom</spam>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i className="fa-solid fa-house"></i>
               <spam className={styles.NavText}>Home</spam>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i className="fa-solid fa-right-to-bracket"></i>
               <spam className={styles.NavText}>Sign in</spam>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
               <i className="fa-solid fa-user-plus"></i>
               <spam className={styles.NavText}>Sign up</spam>
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>

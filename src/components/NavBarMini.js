fcbc6725af9d2704ad74a7661de5e0e9dde3a0df
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-16 21:32:16 +0000

    Add new navbar for smaller screens

---
+++ src/components/NavBarMini.js
@@ -0,0 +1,150 @@
+import React from "react"
+import { Navbar, Container, Nav} from "react-bootstrap";
+import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
+import { useCurrentUser, useSetCurrentUser, } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+
+const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post('dj-rest-auth/logout/');
+      setCurrentUser(null);
+    } catch (err) {
+      // console.log(err);
+    }
+  };
+
+  const addSignOutIcon = (
+    <NavLink
+      className={styles.NavLink}
+      to="/"
+      onClick={handleSignOut}
+      aria-label="signout"
+    >
+      <i className="fa-solid fa-right-from-bracket"></i>
+    </NavLink>
+  );  
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="fa-solid fa-circle-plus"></i>
+    </NavLink>
+  );
+
+  const addEventIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/events/create"
+    >
+      <i className="fa-regular fa-calendar-plus"></i>
+    </NavLink>
+  );
+
+  const addArticleIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/articles/create"
+    >
+      <i className="fa-solid fa-file-circle-plus"></i>
+    </NavLink>
+  );
+
+
+  const loggedInIcons = (
+    <>
+      {currentUser && addEventIcon}
+      {currentUser && addArticleIcon}
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/events"
+      >
+        <i className="fa-regular fa-calendar-days"></i>
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/articles"
+      >
+        <i className="fa-solid fa-file-lines"></i>
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/followers"
+      >
+        <i className="fa-solid fa-users"></i>
+      </NavLink>
+
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
+      </NavLink>
+    </>
+  );
+
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fa-solid fa-right-to-bracket"></i>
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i className="fa-solid fa-user-plus"></i>
+      </NavLink>
+    </>
+  );
+
+  return (
+    <Navbar expand="md" className={`${styles.NavBar}`}>
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img className={styles.logo} src={logo} alt="logo" height="35" />
+          </Navbar.Brand>
+        </NavLink>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-house"></i>
+            </NavLink>
+            {currentUser && addPostIcon}
+            {currentUser ? loggedInIcons : loggedOutIcons}
+            {currentUser && addSignOutIcon}
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar

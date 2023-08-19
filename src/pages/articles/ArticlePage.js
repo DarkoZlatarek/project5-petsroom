47ac92591e565f25b666e433a6deed37e6aef2ff
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-19 19:46:25 +0000

    Add ArticlePage.js

---
+++ src/pages/articles/ArticlePage.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+import { Container, Col, Row } from "react-bootstrap";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import {axiosReq} from "../../api/axiosDefaults"
+
+function PostPage() {
+  const { id } = useParams();
+  const [article, setArticle] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: article }] = await Promise.all([
+          axiosReq.get(`/articles/${id}`),
+        ]);
+        setArticle({ results: [article] });
+        console.log(article);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <p>Article component</p>
+        <Container className={appStyles.Content}>Comments</Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;

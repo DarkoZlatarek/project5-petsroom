d0be2173f2c1c0595742af98f9626049ece64a54
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-19 18:58:43 +0000

    Add EventCreateForm.js and add it to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import EventCreateForm from "./pages/events/EventCreateForm";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/events/create" render={() => <EventCreateForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>

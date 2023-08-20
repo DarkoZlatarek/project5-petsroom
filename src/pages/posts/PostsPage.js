a29a96eb702484ae574ad413474cf020d0783887
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-20 18:52:17 +0000

    Add posts page to render all posts

---
+++ src/pages/posts/PostsPage.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import Post from "./Post"
+import Asset from "../../components/Asset"
+
+import { Form, Col, Row, Container } from "react-bootstrap";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png"
+
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, query, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        <i className={`fa-solid fa-magnifying-glass ${styles.SearchIcon}`}></i>
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;

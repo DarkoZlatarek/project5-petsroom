652f7d0046e94de04d365f12b4a9bd92eda58165
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-22 20:22:29 +0000

    Add popperConfig to Dropdown.Menu

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -20,7 +20,7 @@ export const MoreDropdown = ({handleEdit, handleDelete}) => {
       <Dropdown className="ml-auto" drop="left">
         <Dropdown.Toggle as={ThreeDots} />
 
-        <Dropdown.Menu className="text-center">
+        <Dropdown.Menu className="text-center" popperConfig={{ strategy: "fixed" }}>
           <Dropdown.Item
             className={styles.DropdownItem}
             onClick={handleEdit}

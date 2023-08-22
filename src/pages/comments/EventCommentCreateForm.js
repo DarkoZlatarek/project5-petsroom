72a8e8088cea9feabcfe8de0e8da8a31bbefe33d
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-22 20:21:36 +0000

    Add forms to create comments for posts and events

---
+++ src/pages/comments/EventCommentCreateForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CreateEditCommentsForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function EventCommentCreateForm(props) {
+  const { eventpost, setEventpost, setEventComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/eventcomments/", {
+        content,
+        eventpost,
+      });
+      setEventComments((prevEventComments) => ({
+        ...prevEventComments,
+        results: [data, ...prevEventComments.results],
+      }));
+      setEventpost((prevEventpost) => ({
+        results: [
+          {
+            ...prevEventpost.results[0],
+            eventcomments_count: prevEventpost.results[0].eventcomments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Form}
+            placeholder="my comment..."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${styles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        post
+      </button>
+    </Form>
+  );
+}
+
+export default EventCommentCreateForm;

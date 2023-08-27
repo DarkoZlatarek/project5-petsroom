abaefa3fb9e71570597bab8953af4f5dfdee5026
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-27 11:54:24 +0000

    Add slice for popular profiles for desktop

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -24,7 +24,7 @@ const PopularProfiles = ({ mobile }) => {
               ))}
             </div>
           ) : (
-            popularProfiles.results.map((profile) => (
+            popularProfiles.results.slice(0,5).map((profile) => (
               <Profile key={profile.id} profile={profile} />
             ))
           )}

7ee1e60bb08132f601c7fd1f1c6ad634d8e60cee
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-28 12:31:54 +0000

    Update button style

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -134,7 +134,7 @@ const ProfileEditForm = () => {
               ))}
               <div>
                 <Form.Label
-                  className={`${btnStyles.Button} ${btnStyles.Blue} btn my-auto`}
+                  className={`${btnStyles.Button} ${btnStyles.BlackCancelCreate} btn my-auto`}
                   htmlFor="image-upload"
                 >
                   Change the image

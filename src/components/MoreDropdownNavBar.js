a9b5b8a92c8fbdb9de43b28e810581104f126a50
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-28 15:39:48 +0000

    Add component for profile dropdown in navbar

---
+++ src/components/MoreDropdownNavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
+import { useCurrentUser, useSetCurrentUser, } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+
+
+export function MoreDropdownNavBar({ id }) {
+  
+  const history = useHistory();
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <div ref={ref}
+  onClick={(e) => {
+    e.preventDefault();
+    onClick(e);
+  }}>
+    <Avatar src={currentUser?.profile_image} height={30} />
+  </div>
+));
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`}>
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu className={styles.DropdownMenu} >
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${currentUser?.profile_id}`)}
+          aria-label="profile"
+        >
+           Profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push("/liked")}
+          aria-label="Liked posts"
+        >
+          Liked posts
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={async () => {
+            try {
+              await axios.post('dj-rest-auth/logout/');
+              setCurrentUser(null);
+              history.push("/");
+            } catch (err) {
+              // console.log(err);
+            }
+          }}
+          aria-label="sign out"
+        >
+          Sign out
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
